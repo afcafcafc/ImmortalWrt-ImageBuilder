dc642a93e801c232534b94047ff56679b6dfbe32
afcafcafc <75744716+afcafcafc@users.noreply.github.com>
2026-05-27 21:12:30 +0800

    Update build25.sh

--- rockchip/build25.sh
+++ rockchip/build25.sh
@@ -46,22 +46,94 @@ echo "查看repositories信息——————"
 cat repositories
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
-PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-theme-argon"
-PACKAGES="$PACKAGES luci-app-argon-config"
-PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-# 判断是否需要编译 Docker 插件
-if [ "$INCLUDE_DOCKER" = "yes" ]; then
-    PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
-    echo "Adding package: luci-i18n-dockerman-zh-cn"
-fi
+
+
+packages=(
+  luci-i18n-airplay2-zh-cn
+  luci-i18n-openlist-zh-cn
+  luci-i18n-filemanager-zh-cn
+  luci-app-argon-config
+  luci-i18n-ddns-go-zh-cn
+  luci-i18n-frpc-zh-cn
+  luci-i18n-ttyd-zh-cn
+  luci-i18n-vsftpd-zh-cn
+  luci-i18n-wol-zh-cn
+  luci-i18n-autoreboot-zh-cn
+  luci-i18n-firewall-zh-cn
+  luci-app-argon-config
+  luci-i18n-argon-config-zh-cn
+  luci-i18n-diskman-zh-cn
+  luci-i18n-package-manager-zh-cn
+  luci-i18n-ttyd-zh-cn
+  luci-app-openclash
+  luci-i18n-samba4-zh-cn
+  luci-i18n-dockerman-zh-cn
+  luci-proto-wireguard
+  openssh-sftp-server
+  ppp-mod-pptp
+  script-utils
+  fdisk
+  xl2tpd
+  adb
+  curl
+  ntpdate
+  usbutils
+  python3
+  python3-pip
+  python3-requests  
+  kmod-usb-core
+  kmod-xfrm-interface
+  kmod-ipsec
+  kmod-ipsec4
+  kmod-ipsec6
+  iptables-mod-ipsec
+  iptables-nft
+  kmod-usb2
+  kmod-usb3
+  mt7601u-firmware
+  wireguard-tools
+  #kmod-usb-net-rtl8152
+  kmod-usb-net-asix-ax88179
+  kmod-usb-net-asix
+  kmod-ath9k-htc
+  kmod-mt76
+  kmod-mt7601u
+  kmod-mt76x0u
+  kmod-mt76x2u
+  kmod-mt7921u
+  kmod-usb-ohci
+  kmod-usb-uhci
+  kmod-ata-ahci
+  kmod-ata-core
+  kmod-fs-ntfs
+  kmod-fs-ext4
+  kmod-usb-storage
+  kmod-usb-storage-extras
+  kmod-usb-serial
+  kmod-usb-serial-pl2303
+  kmod-usb-serial-ch341
+  kmod-usb-printer
+  kmod-usb-net-rndis
+  kmod-i2c-core
+  kmod-i2c-gpio
+  kmod-spi-dev
+  kmod-gpio-button-hotplug
+  kmod-leds-gpio
+  kmod-hid
+  kmod-hid-generic
+  kmod-sound-core
+  kmod-usb-audio
+)
+
+# 使用循环逐个追加
+for pkg in "${packages[@]}"; do
+  PACKAGES="$PACKAGES $pkg"
+done
+
+# （可选）去重，防止重复包名
+PACKAGES=$(echo "$PACKAGES" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 # 文件管理器
-PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
+
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"

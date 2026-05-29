9c445e5470159332a1571b4a5b318f80712826e7
afcafcafc <75744716+afcafcafc@users.noreply.github.com>
2026-05-29 16:30:33 +0800

    Update build25.sh

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -45,22 +45,93 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 
 # ============= imm仓库内的插件==============
 # 定义所需安装的包列表 下列插件你都可以自行删减
+# 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
-PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-theme-argon"
-PACKAGES="$PACKAGES luci-app-argon-config"
-PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-#25.12
-PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES openssh-sftp-server"
 
-# 文件管理器
-PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
-# ======== shell/apk-custom-packages.sh =======
-# 合并imm仓库以外的第三方插件 暂时注释
+
+packages=(
+  luci-i18n-airplay2-zh-cn
+  luci-i18n-openlist-zh-cn
+  luci-i18n-filemanager-zh-cn
+  luci-app-argon-config
+  luci-i18n-ddns-go-zh-cn
+  luci-i18n-frpc-zh-cn
+  luci-i18n-ttyd-zh-cn
+  #luci-i18n-vsftpd-zh-cn
+  luci-i18n-wol-zh-cn
+  luci-i18n-autoreboot-zh-cn
+  luci-i18n-firewall-zh-cn
+  luci-app-argon-config
+  luci-i18n-argon-config-zh-cn
+  luci-i18n-diskman-zh-cn
+  luci-i18n-package-manager-zh-cn
+  luci-i18n-ttyd-zh-cn
+  luci-app-openclash
+  #luci-i18n-samba4-zh-cn
+  luci-i18n-dockerman-zh-cn
+  luci-proto-wireguard
+  #openssh-sftp-server
+  ppp-mod-pptp
+  script-utils
+  fdisk
+  xl2tpd
+  curl
+  ntpdate
+  usbutils
+  python3
+  python3-pip
+  python3-requests  
+  kmod-usb-core
+  kmod-xfrm-interface
+  kmod-ipsec
+  kmod-ipsec4
+  kmod-ipsec6
+  iptables-mod-ipsec
+  iptables-nft
+  kmod-usb2
+  kmod-usb3
+  mt7601u-firmware
+  wireguard-tools
+  #kmod-usb-net-rtl8152
+  kmod-usb-net-asix-ax88179
+  kmod-usb-net-asix
+  kmod-ath9k-htc
+  kmod-mt76
+  kmod-mt7601u
+  kmod-mt76x0u
+  kmod-mt76x2u
+  kmod-mt7921u
+  kmod-usb-ohci
+  kmod-usb-uhci
+  kmod-ata-ahci
+  kmod-ata-core
+  kmod-fs-ext4
+  kmod-usb-storage
+  kmod-usb-storage-extras
+  kmod-usb-serial
+  kmod-usb-serial-pl2303
+  kmod-usb-serial-ch341
+  kmod-usb-printer
+  kmod-usb-net-rndis
+  kmod-i2c-core
+  kmod-i2c-gpio
+  kmod-spi-dev
+  kmod-gpio-button-hotplug
+  kmod-leds-gpio
+  kmod-hid
+  kmod-hid-generic
+  kmod-sound-core
+  kmod-usb-audio
+)
+
+# 使用循环逐个追加
+for pkg in "${packages[@]}"; do
+  PACKAGES="$PACKAGES $pkg"
+done
+
+# （可选）去重，防止重复包名
+PACKAGES=$(echo "$PACKAGES" | tr ' ' '\n' | sort -u | tr '\n' ' ')
+
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
 

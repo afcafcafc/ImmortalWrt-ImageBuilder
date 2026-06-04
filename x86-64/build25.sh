fb1c8a6926df9b3b86311e72658e67db7180c1d6
afcafcafc <75744716+afcafcafc@users.noreply.github.com>
2026-06-04 22:17:59 +0800

    Update build25.sh

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -52,7 +52,7 @@ PACKAGES=""
 packages=(
   luci-i18n-airplay2-zh-cn
   luci-i18n-openlist-zh-cn
-  luci-i18n-filemanager-zh-cn
+  #luci-i18n-filemanager-zh-cn
   luci-app-argon-config
   luci-i18n-ddns-go-zh-cn
   luci-i18n-frpc-zh-cn
@@ -70,6 +70,7 @@ packages=(
   #luci-i18n-samba4-zh-cn
   luci-i18n-dockerman-zh-cn
   luci-proto-wireguard
+  openssh-sftp-server
   #openssh-sftp-server
   ppp-mod-pptp
   script-utils
